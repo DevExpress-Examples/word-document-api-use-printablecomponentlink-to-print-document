7485a65cf3bcdca95018418e99aca4e56aa4b8c3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PrintingSystem/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RichEdit_PrintingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_PrintFromServer = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // btn_PrintFromServer
+            // 
+            this.btn_PrintFromServer.Location = new System.Drawing.Point(180, 50);
+            this.btn_PrintFromServer.Name = "btn_PrintFromServer";
+            this.btn_PrintFromServer.Size = new System.Drawing.Size(146, 23);
+            this.btn_PrintFromServer.TabIndex = 2;
+            this.btn_PrintFromServer.Text = "Load and Print";
+            this.btn_PrintFromServer.Click += new System.EventHandler(this.btn_PrintFromServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 130);
+            this.Controls.Add(this.btn_PrintFromServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btn_PrintFromServer;
+    }
+}
+
---
+++ CS/PrintingSystem/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace RichEdit_PrintingSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region #printfromserver
+        private void btn_PrintFromServer_Click(object sender, EventArgs e)
+        {
+            RichEditDocumentServer srv = new RichEditDocumentServer();
+            srv.LoadDocument("Grimm.docx", DocumentFormat.OpenXml);
+            // Insert a field displaying the current date/time into the document header.
+            srv.BeginUpdate();
+            SubDocument  _header = srv.Document.Sections[0].BeginUpdateHeader();
+            _header.Fields.Create(_header.Range.Start,"DATE \\@ \"dddd, MMMM dd, yyyy  HH:mm:ss\" \\MERGEFORMAT");
+            _header.Paragraphs[0].Alignment = ParagraphAlignment.Right;
+            srv.Document.Sections[0].EndUpdateHeader(_header);
+            // Specify page margins, orientation, etc.
+            SetPrintOptions(srv);
+            srv.EndUpdate();
+            // Display field values instead of code.
+            srv.Options.MailMerge.ViewMergedData = true;
+            // Create a printable link to print a document.
+            PrintViaLink(srv);
+        }
+        #endregion #printfromserver
+        #region #setprintoptions
+        private static void SetPrintOptions(IRichEditDocumentServer richedit)
+        {
+            foreach (Section _section in richedit.Document.Sections) {
+                _section.Page.PaperKind = System.Drawing.Printing.PaperKind.A4;
+                _section.Page.Landscape = false;
+                _section.Margins.Left = 150f;
+                _section.Margins.Right = 150f;
+                _section.Margins.Top = 50f;
+                _section.Margins.Bottom = 50f;
+                _section.PageNumbering.NumberingFormat = NumberingFormat.CardinalText;
+                _section.PageNumbering.FirstPageNumber = 0;
+            }
+        }
+        #endregion #setprintoptions
+        #region #printvialink
+        private static void PrintViaLink(RichEditDocumentServer srv)
+        {
+            if (!srv.IsPrintingAvailable) return;
+            PrintableComponentLink link = new PrintableComponentLink(new PrintingSystem());
+            link.Component = srv;
+            // Disable warnings.
+            link.PrintingSystem.ShowMarginsWarning = false;
+            link.PrintingSystem.ShowPrintStatusDialog = false;
+            // Find a printer containing 'Canon' in its name.
+            string printerName = String.Empty;
+            for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++) {
+                string pName = PrinterSettings.InstalledPrinters[i];
+                if (pName.Contains("Canon")) {
+                    printerName = pName;
+                    break;
+                }
+            }
+            // Print to the specified printer.
+            link.Print(printerName);
+        }
+        #endregion #printvialink
+    }
+}
