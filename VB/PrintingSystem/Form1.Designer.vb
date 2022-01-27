92d93a1e5dd79ee75c6251794391cb0e2450e186
DevExpressExampleBot <fakeEmail>
2022-01-27 15:30:35 +0300

    Source auto update [skip ci]

--- VB/PrintingSystem/Form1.Designer.vb
+++ VB/PrintingSystem/Form1.Designer.vb
@@ -1,56 +1,54 @@
-﻿Namespace RichEdit_PrintingSystem
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace RichEdit_PrintingSystem
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.btn_PrintFromServer = New System.Windows.Forms.Button()
-			Me.SuspendLayout()
-			' 
-			' btn_PrintFromServer
-			' 
-			Me.btn_PrintFromServer.Location = New System.Drawing.Point(180, 50)
-			Me.btn_PrintFromServer.Name = "btn_PrintFromServer"
-			Me.btn_PrintFromServer.Size = New System.Drawing.Size(146, 23)
-			Me.btn_PrintFromServer.TabIndex = 2
-			Me.btn_PrintFromServer.Text = "Load and Print"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.btn_PrintFromServer.Click += new System.EventHandler(this.btn_PrintFromServer_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(506, 130)
-			Me.Controls.Add(Me.btn_PrintFromServer)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.btn_PrintFromServer = New System.Windows.Forms.Button()
+            Me.SuspendLayout()
+            ' 
+            ' btn_PrintFromServer
+            ' 
+            Me.btn_PrintFromServer.Location = New System.Drawing.Point(180, 50)
+            Me.btn_PrintFromServer.Name = "btn_PrintFromServer"
+            Me.btn_PrintFromServer.Size = New System.Drawing.Size(146, 23)
+            Me.btn_PrintFromServer.TabIndex = 2
+            Me.btn_PrintFromServer.Text = "Load and Print"
+            AddHandler Me.btn_PrintFromServer.Click, New System.EventHandler(AddressOf Me.btn_PrintFromServer_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(506, 130)
+            Me.Controls.Add(Me.btn_PrintFromServer)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents btn_PrintFromServer As System.Windows.Forms.Button
-	End Class
+#End Region
+        Private btn_PrintFromServer As System.Windows.Forms.Button
+    End Class
 End Namespace
-
